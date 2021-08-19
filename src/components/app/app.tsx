da78d643e0a2ea82c4f1a0f0a5245f4e6ff524d0
Jalolov Sultonali <s.jalolov@hlsys.uz>
2021-08-19 18:31:47 +0500

    feat: change searchbar position

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,154 +1,36 @@
-import React, {Component} from 'react';
-import SearchBar from '../searchbar/searchbar';
+import React from 'react';
+import {useTransition, animated} from 'react-spring';
+import {Route, Switch, useLocation} from 'react-router-dom';
 
+import Header from '../header/header';
+import HomePage from '../../pages/home/homePage';
+import About from '../../pages/about/about';
 import './app.scss';
-import {NewsApiService} from '../../services/newsApiService/newsApiService';
-import {NewsCardWrapper} from '../newsCard/newsCard';
-import {AppState, IFilter} from '../../models/stateModels';
-import Pagination from '../pagination/pagination';
 
-export default class App extends Component {
-  state: AppState = {
-    data: {
-      status: '',
-      totalResults: 0,
-      articles: [],
-    },
-    error: {
-      status: '',
-      code: '',
-      message: '',
-    },
-    loading: false,
-    searchWord: '',
-    perPage: '',
-    filter: {
-      language: '',
-      sortType: '',
-      from: '',
-      to: '',
-    },
-  };
-
-  private newsApiService: NewsApiService = new NewsApiService();
-
-  onSearch = (value: string, filter: IFilter): void => {
-    if (!value.trim()) {
-      alert('Search field is incorrect');
-      return;
-    }
-    this.setState({
-      loading: true,
-      searchWord: value,
-      filter,
-    });
-    this.newsApiService
-      .getNews(value, {filter, perPage: Number(this.state.perPage)})
-      .then((res) => res.json())
-      .then((data) => {
-        if (data?.status === 'ok') {
-          this.setState({
-            data: data,
-            error: {},
-            loading: false,
-          });
-        } else {
-          this.setState({
-            articles: [],
-            error: data,
-            loading: false,
-          });
-        }
-      });
-  };
-
-  onPageChange = (value: number): void => {
-    if (!this.state.searchWord) {
-      return;
-    }
-    this.setState({
-      loading: true,
-    });
-    this.newsApiService
-      .getNews(this.state.searchWord, {
-        filter: this.state.filter,
-        page: value,
-      })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data?.status === 'ok') {
-          this.setState({
-            data: data,
-            error: {},
-            loading: false,
-          });
-        } else {
-          this.setState({
-            articles: [],
-            error: data,
-            loading: false,
-          });
-        }
-      });
-  };
-
-  onPerPageChange = (perPageCount: string): void => {
-    if (!this.state.searchWord) {
-      this.setState({
-        perPage: perPageCount,
-      });
-      return;
-    }
-    this.setState({
-      perPage: perPageCount,
-      loading: true,
-    });
-    this.newsApiService
-      .getNews(this.state.searchWord, {
-        filter: this.state.filter,
-        page: 1,
-        perPage: Number(perPageCount),
-      })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data?.status === 'ok') {
-          this.setState({
-            data: data,
-            error: {},
-            loading: false,
-          });
-        } else {
-          this.setState({
-            articles: [],
-            error: data,
-            loading: false,
-          });
-        }
-      });
-  };
-
-  render(): JSX.Element {
-    return (
-      <div className="app">
-        <SearchBar
-          buttonText="Search"
-          placeholder="Enter search text"
-          onSearch={this.onSearch}
-          onPerPageChange={(value: string) => this.onPerPageChange(value)}
-        />
-        <NewsCardWrapper
-          data={this.state.data}
-          error={this.state.error}
-          loading={this.state.loading}
-        />
-        {this.state.data.totalResults > 0 && (
-          <Pagination
-            totalResults={this.state.data.totalResults}
-            perPage={Number(this.state.perPage)}
-            onPageChange={this.onPageChange}
-          />
-        )}
-      </div>
-    );
-  }
+function App(): JSX.Element {
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: {opacity: 0, transform: 'translate3d(100vw, 0, 0)'},
+    enter: {opacity: 1, transform: 'translate3d(0, 0, 0)'},
+    leave: {opacity: 0, transform: 'translate3d(-20vw, 0, 0)'},
+  });
+  return (
+    <div className="app">
+      <Header />
+      {transitions((props, item) => (
+        <animated.div className="app-main" style={props}>
+          <Switch location={item}>
+            <Route path="/" exact>
+              <HomePage />
+            </Route>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+          </Switch>
+        </animated.div>
+      ))}
+    </div>
+  );
 }
+
+export default App;
---
+++ src/pages/home/homePage.tsx
@@ -0,0 +1,155 @@
+import React, {Component} from 'react';
+import {AppState, IFilter} from '../../models/stateModels';
+import {NewsApiService} from '../../services/newsApiService/newsApiService';
+import SearchBar from '../../components/searchbar/searchbar';
+import {NewsCardWrapper} from '../../components/newsCard/newsCard';
+import Pagination from '../../components/pagination/pagination';
+
+class HomePage extends Component {
+  state: AppState = {
+    data: {
+      status: '',
+      totalResults: 0,
+      articles: [],
+    },
+    error: {
+      status: '',
+      code: '',
+      message: '',
+    },
+    loading: false,
+    searchWord: '',
+    perPage: '',
+    filter: {
+      language: '',
+      sortType: '',
+      from: '',
+      to: '',
+    },
+  };
+
+  private newsApiService: NewsApiService = new NewsApiService();
+
+  onSearch = (value: string, filter: IFilter): void => {
+    if (!value.trim()) {
+      alert('Search field is incorrect');
+      return;
+    }
+    this.setState({
+      loading: true,
+      searchWord: value,
+      filter,
+    });
+    this.newsApiService
+      .getNews(value, {filter, perPage: Number(this.state.perPage)})
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.status === 'ok') {
+          this.setState({
+            data: data,
+            error: {},
+            loading: false,
+          });
+        } else {
+          this.setState({
+            articles: [],
+            error: data,
+            loading: false,
+          });
+        }
+      });
+  };
+
+  onPageChange = (value: number): void => {
+    if (!this.state.searchWord) {
+      return;
+    }
+    this.setState({
+      loading: true,
+    });
+    this.newsApiService
+      .getNews(this.state.searchWord, {
+        filter: this.state.filter,
+        page: value,
+        perPage: Number(this.state.perPage),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.status === 'ok') {
+          this.setState({
+            data: data,
+            error: {},
+            loading: false,
+          });
+        } else {
+          this.setState({
+            articles: [],
+            error: data,
+            loading: false,
+          });
+        }
+      });
+  };
+
+  onPerPageChange = (perPageCount: string): void => {
+    if (!this.state.searchWord) {
+      this.setState({
+        perPage: perPageCount,
+      });
+      return;
+    }
+    this.setState({
+      perPage: perPageCount,
+      loading: true,
+    });
+    this.newsApiService
+      .getNews(this.state.searchWord, {
+        filter: this.state.filter,
+        page: 1,
+        perPage: Number(perPageCount),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data?.status === 'ok') {
+          this.setState({
+            data: data,
+            error: {},
+            loading: false,
+          });
+        } else {
+          this.setState({
+            articles: [],
+            error: data,
+            loading: false,
+          });
+        }
+      });
+  };
+
+  render(): JSX.Element {
+    return (
+      <div className="home-page">
+        <SearchBar
+          buttonText="Search"
+          placeholder="Enter search text"
+          onSearch={this.onSearch}
+          onPerPageChange={(value: string) => this.onPerPageChange(value)}
+        />
+        <NewsCardWrapper
+          data={this.state.data}
+          error={this.state.error}
+          loading={this.state.loading}
+        />
+        {this.state.data.totalResults > 0 && (
+          <Pagination
+            totalResults={this.state.data.totalResults}
+            perPage={Number(this.state.perPage) || 20}
+            onPageChange={this.onPageChange}
+          />
+        )}
+      </div>
+    );
+  }
+}
+
+export default HomePage;
